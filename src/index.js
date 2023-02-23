6bdb17ce2c8858c42e5f4647770fa26207ac4558
Egor Perfiliev <perfilev.egor2016@gmail.com>
2023-02-23 21:52:42 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBr = [];
+  for (i = 0; i < bracketsConfig.length; i++) {
+    openBr.push(bracketsConfig[i][0]);
+  }
+  const closeBr = [];
+  for (i = 0; i < bracketsConfig.length; i++) {
+    closeBr.push(bracketsConfig[i][1]);
+  }
+  const bracketsPair = {};
+  for (i = 0; i < bracketsConfig.length; i++) {
+    let key = bracketsConfig[i][1];
+    bracketsPair[key] = bracketsConfig[i][0];
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const currentEl = str[i];
+    if (openBr.includes(currentEl)) {
+      if (
+        closeBr.includes(currentEl) &&
+        stack[stack.length - 1] === currentEl
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currentEl);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+        console.log("if (stack.length === 0)");
+      }
+      let topElement = stack[stack.length - 1];
+      if (bracketsPair[currentEl] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+        console.log("topElement");
+      }
+    }
+  }
+  return stack.length === 0;
+};
